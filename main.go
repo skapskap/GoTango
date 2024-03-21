aae058ef41f4f6a033b68b10fcab15210d723e61
skapskap <skaprange@gmail.com>
2024-03-21 13:59:31 -0300

    feat: upgrade chat server architecture (registering/unregistering clients and channels)

--- main.go
+++ main.go
@@ -9,12 +9,97 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type Client struct {
+	conn *websocket.Conn
+}
+
+type Message struct {
+	author  *Client
+	content []byte
+}
+
 type Server struct {
-	upgrader websocket.Upgrader
+	router     *chi.Mux
+	upgrader   websocket.Upgrader
+	clients    []*Client
+	message    chan Message
+	register   chan *Client
+	unregister chan *Client
+}
+
+func (c *Client) write(message []byte) {
+	c.conn.WriteMessage(websocket.TextMessage, message)
+}
+
+func NewClient(conn *websocket.Conn) *Client {
+	return &Client{conn: conn}
+}
+
+func (s *Server) listen() {
+	for {
+		select {
+		case message := <-s.message:
+			for _, client := range s.clients {
+				if client != message.author {
+					client.write(message.content)
+				}
+			}
+		case client := <-s.register:
+			s.clients = append(s.clients, client)
+		case client := <-s.unregister:
+			for i, c := range s.clients {
+				if c == client {
+					s.clients = append(s.clients[:i], s.clients[i+1:]...)
+					break
+				}
+			}
+		}
+	}
+}
+
+func readMessages(server *Server, client *Client) {
+	for {
+		_, message, err := client.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err) {
+				log.Println("client disconnected")
+				server.unregister <- client
+				return
+			}
+			log.Println("Unexpected error")
+			log.Println(err)
+			continue
+		}
+
+		log.Println("message received: ", string(message))
+		server.message <- Message{author: client, content: message}
+	}
+}
+
+
+func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("New incoming connection from client!")
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+        log.Println("Failed to upgrade connection")
+		log.Println(err)
+		return
+	}
+	
+	log.Println("client connected")
+
+	client := NewClient(conn)
+	s.register <- client
+
+	go readMessages(s, client)
 }
 
 func NewUpgrader() *Server {
     s := &Server{}
+
+    s.router = chi.NewRouter()
+    s.router.Get("/ws", s.handleWS)
+
     s.upgrader = websocket.Upgrader{
         ReadBufferSize:  1024,
         WriteBufferSize: 1024,
@@ -24,40 +109,17 @@ func NewUpgrader() *Server {
         },
     }
 
-    return s
-}
+    s.message = make(chan Message)
+	s.register = make(chan *Client)
+	s.unregister = make(chan *Client)
 
-func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("New incoming connection from client!")
-	conn, err := s.upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	
-	defer conn.Close()
-
-    _, message, err := conn.ReadMessage()
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    fmt.Println("Message received:", string(message))
-
-    err = conn.WriteMessage(websocket.TextMessage, message)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    fmt.Println("Message sent:", string(message))
+	return s
 }
 
 func main() {
 	server := NewUpgrader()
-
-	r := chi.NewRouter()
-	r.Get("/ws", server.handleWS)
+    go server.listen()
 
 	fmt.Println("Server is running on port 4869")
-	log.Fatal(http.ListenAndServe(":4869", r))
+	log.Fatal(http.ListenAndServe(":4869", server.router))
 }
